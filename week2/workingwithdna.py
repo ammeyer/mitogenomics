cb376178bd9c3b56de3682c03f37841f28da2b42
ammeyer <ammeyer@wesleyan.edu>
2016-10-31 14:29:37 -0400

    Create workingwithdna.py

---
+++ week2/workingwithdna.py
@@ -0,0 +1,25 @@
+# workingwithdna.py
+def count_bases(sequence):
+	"""
+	Returns a count of how many of each nucleotide (A,C,T,G) exist in the DNA sequence.
+	"""
+	pass
+
+def complement(sequence):
+	"""
+	Returns the reverse complement of a sequence of DNA.
+	"""
+	pass
+
+def transcribe_dna(sequence):
+	"""
+	Returns a string representing the mRNA transcript of a DNA sequence
+	"""
+	pass
+
+def translate_rna(sequence):
+	"""
+	Returns a string representing the amino acid translation of an mRNA
+	sequence
+	"""
+	pass
