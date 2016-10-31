e52addff446c0695f8e8c0a15353b6ef46866e84
ammeyer <ammeyer@wesleyan.edu>
2016-10-31 14:32:23 -0400

    Update workingwithdna.py

--- week2/workingwithdna.py
+++ week2/workingwithdna.py
@@ -1,16 +1,35 @@
 # workingwithdna.py
+def count_length(sequence):
+	"""
+	Returns a integer value for the length of the sequence.
+	"""
+	pass
+
 def count_bases(sequence):
 	"""
 	Returns a count of how many of each nucleotide (A,C,T,G) exist in the DNA sequence.
 	"""
 	pass
 
+def compare_lengths(sequence1, sequence2):
+	"""
+	Returns the longer sequence.
+	"""
+	pass
+
+
+# if we have time
 def complement(sequence):
 	"""
 	Returns the reverse complement of a sequence of DNA.
 	"""
 	pass
 
+
+
+
+
+# next time
 def transcribe_dna(sequence):
 	"""
 	Returns a string representing the mRNA transcript of a DNA sequence
